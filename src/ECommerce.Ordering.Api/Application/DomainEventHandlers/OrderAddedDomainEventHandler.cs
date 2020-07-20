f301291533941e0b52108bb1df23afda117a8328
raphaelcastilhoc <raphael.castilhoc@gmail.com>
2020-07-20 09:30:07 -0300

    Implementing OrderAddedDomainEventHandler

--- src/ECommerce.Ordering.Api/Application/DomainEventHandlers/OrderAddedDomainEventHandler.cs
+++ src/ECommerce.Ordering.Api/Application/DomainEventHandlers/OrderAddedDomainEventHandler.cs
@@ -1,4 +1,6 @@
-﻿using ECommerce.Ordering.Domain.Events;
+﻿using ECommerce.EventBus;
+using ECommerce.Ordering.Api.Application.IntegrationEvents;
+using ECommerce.Ordering.Domain.Events;
 using MediatR;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,8 +9,19 @@ namespace ECommerce.Ordering.Api.Application.DomainEventHandlers
 {
     public class OrderAddedDomainEventHandler : INotificationHandler<OrderAddedDomainEvent>
     {
-        public async Task Handle(OrderAddedDomainEvent orderAddedDomainEvent, CancellationToken cancellationToken)
+        private readonly IEventBus _eventBus;
+
+        public OrderAddedDomainEventHandler(IEventBus eventBus)
         {
+            _eventBus = eventBus;
+        }
+
+        public Task Handle(OrderAddedDomainEvent orderAddedDomainEvent, CancellationToken cancellationToken)
+        {
+            var orderAddedEvent = new OrderAddedEvent(orderAddedDomainEvent.ProductId, orderAddedDomainEvent.Quantity);
+            _eventBus.Publish(orderAddedEvent);
+
+            return Task.CompletedTask;
         }
     }
 }
---
+++ src/ECommerce.Ordering.Api/Application/IntegrationEvents/OrderAddedEvent.cs
@@ -0,0 +1,17 @@
+﻿using ECommerce.EventBus;
+
+namespace ECommerce.Ordering.Api.Application.IntegrationEvents
+{
+    public class OrderAddedEvent : IntegrationEvent
+    {
+        public OrderAddedEvent(int productId, int quantity)
+        {
+            ProductId = productId;
+            Quantity = quantity;
+        }
+
+        public int ProductId { get; }
+
+        public int Quantity { get; }
+    }
+}
